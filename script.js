c9b73ec2bb1ea6c6f2030338310885a9723379b0
Willykgafela <willytshepho@gmail.com>
2026-01-20 12:31:13 +0200

    update
    update

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ window.onscroll = () => {
     backToTop.style.display = window.scrollY > 300 ? "block" : "none";
   }
 };
-};
+;
 
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
