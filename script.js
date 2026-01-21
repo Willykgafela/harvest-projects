d71cb0420408d490586a25be1067df41e5484ef0
Willykgafela <willytshepho@gmail.com>
2026-01-21 09:26:10 +0200

    upgrade
    upgrade

--- script.js
+++ script.js
@@ -83,3 +83,9 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.getElementById("nav");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
